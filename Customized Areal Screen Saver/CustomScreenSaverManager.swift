43c85493a78913334093ad0d79cbe0bc2afffc9b
falcon <falcon.lee1011@gmail.com>
2025-06-28 17:12:08 +0800

    feat: macOS 26 Tahoe support (beta)

--- Customized Areal Screen Saver/CustomScreenSaverManager.swift
+++ Customized Areal Screen Saver/CustomScreenSaverManager.swift
@@ -18,9 +18,15 @@ struct AssetsURLCollection{
     let customAssetsEntriesURL: URL
 }
 
+enum TahoeUpgradeError: Error {
+    case UnknownError
+}
+
 class CustomScreenSaverManager: ObservableObject {
     
     @Published var customAerialCategorySubcategories: Array<TVIdleScreenEntryCategorySubcategory> = []
+    
+    private let userDefaults = UserDefaults(suiteName: "com.xtl.customizedarealscreensaver")
     
     private let fileManager: FileManager = FileManager()
     private let customAerialCategoryUUID: String = "C8EE7C2A-8025-4094-91C2-9637B6D2A64D"
@@ -31,34 +37,75 @@ class CustomScreenSaverManager: ObservableObject {
     private var TVIdleScreenEntries: TVIdleScreenEntry?
     private var customAerialCategoryIndex: Int = -1
     
+    private let OSVersion = ProcessInfo().operatingSystemVersion.majorVersion
+    private var previewImageExtension = "png"
+    private var previewImagePrefix = ""
     
-    private let defaultSystemAssetsRootPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .systemDomainMask).first!.appending(path: "com.apple.idleassetsd").path(percentEncoded: false)
+    
+    // Before macOS 26
+    private let legacyDefaultSystemAssetsRootPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .systemDomainMask).first!.appending(path: "com.apple.idleassetsd").path(percentEncoded: false)
+    private let legacyDefaultCustomAssetsRootPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!.appending(path: "com.xtl.customizedarealscreensaver").path(percentEncoded: false)
+    
+    // macOS 26
+    private let defaultSystemAssetsRootPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!.appending(path: "com.apple.wallpaper").path(percentEncoded: false)
     private let defaultCustomAssetsRootPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!.appending(path: "com.xtl.customizedarealscreensaver").path(percentEncoded: false)
     
-    init(
-        systemAssetsRootPath: String? = nil,
-        customAssetsRootPath: String? = nil
-    ) {
-        let _systemAssetsRootPath = systemAssetsRootPath ?? defaultSystemAssetsRootPath
-        let _customAssetsRootPath = customAssetsRootPath ?? defaultCustomAssetsRootPath
+    init() {
         
+        var _systemAssetsRootPath = defaultSystemAssetsRootPath
+        var _customAssetsRootPath = defaultCustomAssetsRootPath
+        var systemAssetsRootURL = URL(filePath: _systemAssetsRootPath)
+        var customAssetsRootURL = URL(filePath: _customAssetsRootPath)
         
-        let systemAssetsRootURL = URL(fileURLWithPath: _systemAssetsRootPath)
-        let customAssetsRootURL = URL(fileURLWithPath: _customAssetsRootPath)
-        
-        print(systemAssetsRootURL.path(percentEncoded: false))
-        print(customAssetsRootURL.path(percentEncoded: false))
-        
-        self.assetsURLCollection = AssetsURLCollection(
-            systemAssetsRootURL: systemAssetsRootURL,
-            systemAssetsPreviewURL: systemAssetsRootURL.appending(component: "snapshots"),
-            systemAssetsVideoURL: systemAssetsRootURL.appending(component: "Customer"),
-            systemAssetsEntriesURL: systemAssetsRootURL.appending(component: "Customer/entries.json"),
-            customAssetsRootURL: customAssetsRootURL,
-            customAssetsPreviewURL: customAssetsRootURL.appending(component: "asset-preview"),
-            customAssetsVideoURL: customAssetsRootURL.appending(component: "video"),
-            customAssetsEntriesURL: customAssetsRootURL.appending(component: "entries.json")
-        )
+        if (self.OSVersion <= 15) {
+            _systemAssetsRootPath = legacyDefaultSystemAssetsRootPath
+            _customAssetsRootPath = legacyDefaultCustomAssetsRootPath
+            self.previewImageExtension = "jpg"
+            systemAssetsRootURL = URL(filePath: _systemAssetsRootPath)
+            customAssetsRootURL = URL(filePath: _customAssetsRootPath)
+            self.previewImagePrefix = "asset-preview-"
+            
+            self.assetsURLCollection = AssetsURLCollection(
+                systemAssetsRootURL: systemAssetsRootURL,
+                systemAssetsPreviewURL: systemAssetsRootURL.appending(component: "snapshots"),
+                systemAssetsVideoURL: systemAssetsRootURL.appending(component: "Customer/4KSDR240FPS"),
+                systemAssetsEntriesURL: systemAssetsRootURL.appending(component: "Customer/entries.json"),
+                
+                customAssetsRootURL: customAssetsRootURL,
+                customAssetsPreviewURL: customAssetsRootURL.appending(component: "asset-preview"),
+                customAssetsVideoURL: customAssetsRootURL.appending(component: "video"),
+                customAssetsEntriesURL: customAssetsRootURL.appending(component: "entries.json")
+            )
+        } else {
+            systemAssetsRootURL = URL(filePath: _systemAssetsRootPath)
+            customAssetsRootURL = URL(filePath: _customAssetsRootPath)
+            
+            self.assetsURLCollection = AssetsURLCollection(
+                systemAssetsRootURL: systemAssetsRootURL,
+                systemAssetsPreviewURL: systemAssetsRootURL.appending(component: "aerials/thumbnails"),
+                systemAssetsVideoURL: systemAssetsRootURL.appending(component: "aerials/videos"),
+                systemAssetsEntriesURL: systemAssetsRootURL.appending(component: "aerials/manifest/entries.json"),
+                
+                customAssetsRootURL: customAssetsRootURL,
+                customAssetsPreviewURL: customAssetsRootURL.appending(component: "asset-preview"),
+                customAssetsVideoURL: customAssetsRootURL.appending(component: "video"),
+                customAssetsEntriesURL: customAssetsRootURL.appending(component: "entries.json")
+            )
+            
+            userDefaults?.set(false, forKey: "upgraded")
+            if ((userDefaults?.bool(forKey: "upgraded") ?? false) == false) {
+                do{
+                    let upgradeResult = self._upgradeToTahoe()
+                    if (!upgradeResult) {
+                        throw TahoeUpgradeError.UnknownError
+                    } else {
+                        userDefaults?.set(true, forKey: "upgraded")
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
         
         var isDir: ObjCBool = true
         if (!self.fileManager.fileExists(atPath: _customAssetsRootPath, isDirectory: &isDir)){
@@ -70,6 +117,74 @@ class CustomScreenSaverManager: ObservableObject {
         self._checkCustomAerialCategoryExists()
         
         self._fixMispelledPath(customAssetsRootURL: customAssetsRootURL)
+    }
+    
+    private func _upgradeToTahoe() -> Bool{
+        var upgradeSucceed = true
+        
+        // Path upgrade
+        if let enumerator = self.fileManager.enumerator(at: self.assetsURLCollection.customAssetsPreviewURL, includingPropertiesForKeys: [.isRegularFileKey]) {
+            for case let fileURL as URL in enumerator {
+                if (fileURL.path().hasSuffix(".jpg") || fileURL.path().hasSuffix(".jpeg")) {
+                    do {
+                        let newURLString = fileURL
+                            .deletingPathExtension()
+                            .appendingPathExtension("png")
+                            .path(percentEncoded: false)
+                            .replacingOccurrences(of: "asset-preview-", with: "")
+                        
+                        try self.fileManager.moveItem(atPath: fileURL.path(percentEncoded: false), toPath: newURLString)
+                    } catch {
+                        print("Error renaming \(fileURL)")
+                        print(error)
+                        upgradeSucceed = false
+                    }
+                }
+            }
+        }
+        
+        // Entries upgrade
+        
+        self._loadEntriesConfig()
+        
+        var assetIds: Set<String> = []
+        
+        func _renamePreviewAsset (_ original: String) -> String {
+            return original
+                .replacingOccurrences(of: "asset-preview-", with: "")
+                .replacingOccurrences(of: ".jpg", with: ".png")
+        }
+        
+        let updatedCategories = self.TVIdleScreenEntries?.categories.map({ category in
+            var updatedCategory = category
+            print(category.id)
+            print(self.customAerialCategoryUUID)
+            print()
+            if (category.id == self.customAerialCategoryUUID) {
+                updatedCategory.subcategories = category.subcategories.map({ subcategory in
+                    var updated = subcategory
+                    updated.previewImage = _renamePreviewAsset(updated.previewImage)
+                    assetIds.insert(subcategory.representativeAssetID)
+                    return updated
+                })
+            }
+            return updatedCategory
+        })
+        
+        let updatedAssets = self.TVIdleScreenEntries?.assets.map({ asset in
+            var updatedAsset = asset
+            if (assetIds.contains(asset.id)) {
+                updatedAsset.previewImage = _renamePreviewAsset(updatedAsset.previewImage)
+            }
+            return updatedAsset
+        })
+        
+        self.TVIdleScreenEntries?.categories = updatedCategories!
+        self.TVIdleScreenEntries?.assets = updatedAssets!
+        
+        self._saveEntryChanges()
+        
+        return upgradeSucceed
     }
     
     private func _fixMispelledPath(customAssetsRootURL: URL){
@@ -111,9 +226,6 @@ class CustomScreenSaverManager: ObservableObject {
                 })
                 self._saveEntryChanges()
                 self.refreshSystemAssetd()
-            }
-            catch {
-                print("Failed to fix misspelled entries or subcategories")
             }
         }
     }
@@ -204,15 +316,15 @@ class CustomScreenSaverManager: ObservableObject {
         let newAssetID = newEntryAsset.id
         do {
             try fileManager.copyItem(
-                at: URL(fileURLWithPath: originalAssetPath),
-                to: URL(fileURLWithPath: newEntryAsset.url4KSDR240FPS)
+                at: URL(filePath: originalAssetPath),
+                to: URL(filePath: newEntryAsset.url4KSDR240FPS)
             )
             try fileManager.copyItem(
-                at: URL(fileURLWithPath: originalAsserPreviewPath),
-                to: URL(fileURLWithPath: newEntryAsset.previewImage)
+                at: URL(filePath: originalAsserPreviewPath),
+                to: URL(filePath: newEntryAsset.previewImage)
             )
-            self._copyItemWithCommand(from: URL(fileURLWithPath: originalAsserPreviewPath), to: self.assetsURLCollection.systemAssetsPreviewURL.appending(path: "asset-preview-\(newEntryAsset.id).jpg"))
-            self._copyItemWithCommand(from: URL(fileURLWithPath: originalAssetPath), to: self.assetsURLCollection.systemAssetsVideoURL.appending(path: "4KSDR240FPS/\(newAssetID).mov"))
+            self._copyItemWithCommand(from: URL(filePath: originalAsserPreviewPath), to: self.assetsURLCollection.systemAssetsPreviewURL.appending(path: "\(self.previewImagePrefix)\(newEntryAsset.id).\(self.previewImageExtension)"))
+            self._copyItemWithCommand(from: URL(filePath: originalAssetPath), to: self.assetsURLCollection.systemAssetsVideoURL.appending(path: "\(newAssetID).mov"))
         } catch {
             print("Error when creating asset.")
             print(error)
@@ -231,6 +343,9 @@ class CustomScreenSaverManager: ObservableObject {
         } catch {
             print(error)
         }
+        self.customAerialCategoryIndex = self.TVIdleScreenEntries!.categories.firstIndex { category in
+            category.id == self.customAerialCategoryUUID
+        } ?? -1
         self._updateCustomAerialCategorySubcategories()
         self.refreshSystemAssetd()
     }
@@ -307,7 +422,7 @@ class CustomScreenSaverManager: ObservableObject {
         let subcategoryID = UUID().uuidString
         
         let customAssetPath = self.assetsURLCollection.customAssetsVideoURL.appending(path: "\(assetID).mov").path(percentEncoded: false)
-        let customAssetPreviewPath = self.assetsURLCollection.customAssetsPreviewURL.appending(path: "asset-preview-\(assetID).jpg").path(percentEncoded: false)
+        let customAssetPreviewPath = self.assetsURLCollection.customAssetsPreviewURL.appending(path: "\(self.previewImagePrefix)\(assetID).\(self.previewImageExtension)").path(percentEncoded: false)
         
         let newEntryAsset = TVIdleScreenEntryAsset(
             localizedNameKey: screenSaverName,
@@ -345,10 +460,10 @@ class CustomScreenSaverManager: ObservableObject {
         
         if (!soft) {
             do {
-                self._removeItemWithCommand(at: self.assetsURLCollection.systemAssetsVideoURL.appending(path: "4KSDR240FPS/\(id).mov"))
-                self._removeItemWithCommand(at: self.assetsURLCollection.systemAssetsPreviewURL.appending(path: "asset-preview-\(id).jpg"))
+                self._removeItemWithCommand(at: self.assetsURLCollection.systemAssetsVideoURL.appending(path: "\(id).mov"))
+                self._removeItemWithCommand(at: self.assetsURLCollection.systemAssetsPreviewURL.appending(path: "\(self.previewImagePrefix)\(id).\(self.previewImageExtension)"))
                 try self.fileManager.removeItem(at: self.assetsURLCollection.customAssetsVideoURL.appending(path: "\(id).mov"))
-                try self.fileManager.removeItem(at: self.assetsURLCollection.customAssetsPreviewURL.appending(path: "asset-preview-\(id).jpg"))
+                try self.fileManager.removeItem(at: self.assetsURLCollection.customAssetsPreviewURL.appending(path: "\(self.previewImagePrefix)\(id).\(self.previewImageExtension)"))
             } catch {
                 print(error)
             }
@@ -364,13 +479,13 @@ class CustomScreenSaverManager: ObservableObject {
         )
         self._copyItemWithCommand(
             from: URL(string: "\(self.assetsURLCollection.customAssetsVideoURL)/*")!,
-            to: URL(string: "\(self.assetsURLCollection.systemAssetsVideoURL)/4KSDR240FPS/")!
+            to: URL(string: "\(self.assetsURLCollection.systemAssetsVideoURL)/")!
         )
         
         do{
             let newData = try Data(contentsOf: self.assetsURLCollection.systemAssetsEntriesURL, options: .mappedIfSafe)
             var newDataEntry = try JSONDecoder().decode(TVIdleScreenEntry.self, from: newData)
-
+            
             let customData = try Data(contentsOf: self.assetsURLCollection.customAssetsEntriesURL, options: .mappedIfSafe)
             let customDataEntry = try JSONDecoder().decode(TVIdleScreenEntry.self, from: customData)
             
